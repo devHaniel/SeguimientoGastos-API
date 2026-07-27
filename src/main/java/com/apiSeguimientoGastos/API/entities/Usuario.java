27548417134b0833cc31c00bde0db43c27745b95
devHaniel <haniel06hernandez@gmail.com>
2026-07-27 16:42:33 -0600

    Actualizar email para que sea unique

--- src/main/java/com/apiSeguimientoGastos/API/entities/Usuario.java
+++ src/main/java/com/apiSeguimientoGastos/API/entities/Usuario.java
@@ -1,5 +1,6 @@
 package com.apiSeguimientoGastos.API.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id; // mismo UUID que genera Supabase Auth
+    @Column(unique = true)
     private String email;
     private String nombre;
     private String passwordHash;
